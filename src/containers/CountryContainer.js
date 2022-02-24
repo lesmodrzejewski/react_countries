573727fa0cec2e424904789ffabad31a80a9aabe
Leszek (Les) <leszek.modrzejewski@gmail.com>
2022-02-24 13:44:18 +0000

    CountryContainer.js started

--- src/containers/CountryContainer.js
+++ src/containers/CountryContainer.js
@@ -1,4 +1,15 @@
+import { useState, useEffect } from "react";
+
 const CountryContainer = () => {
+
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        getCountries();
+    }, [])
+
+
+    
     return ( 
         <h2>This is CountryContainer</h2>
      );
